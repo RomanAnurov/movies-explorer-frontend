51c62c4e4219907bf7fae505bcfbec80449a1a63
Roman Anurov <anurovroman@yandex.ru>
2023-05-04 18:15:32 +0300

    fix:BEM

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,10 +7,10 @@ import './Navigation.css';
 
 function Navigation() {
   return (
-    <><div className="nav-links">
-      <Link className="nav-link" to="/movies">Фильмы</Link>
-      <Link className="nav-link" to="/saved-movies">Сохранённые фильмы</Link>
-      <Link className="nav-link" to="/profile"><img src={LogoProfile} alt="логотип" className="logoprofile" /></Link>
+    <><div className="navigation">
+      <Link className="navigation__link" to="/movies">Фильмы</Link>
+      <Link className="navigation__link" to="/saved-movies">Сохранённые фильмы</Link>
+      <Link className="navigation__link" to="/profile"><img src={LogoProfile} alt="логотип" className="navigation__logo" /></Link>
     </div></>
 
   );
