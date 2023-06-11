29b858811a6a5cd3478f11779f0c0dbc3709aa10
Roman Anurov <anurovroman@yandex.ru>
2023-06-11 16:52:47 +0300

    fix:footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ function Footer(){
         <p className='footer__copyright'>© 2023</p>
         <div className='footer__links'>
           <a className='footer__link' href="https://practicum.yandex.ru/" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
-          <a className='footer__link' href="https://github.com/" target="_blank" rel="noreferrer">Github</a>
+          <a className='footer__link' href="https://github.com/RomanAnurov" target="_blank" rel="noreferrer">Github</a>
         </div>
       </div>
 
